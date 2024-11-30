f55cbb91e9bd4b51201f651678702952dca0a0ae
tiray972 <tiray972@gmail.com>
2024-11-30 12:10:47 +0100

    update: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,19 +16,19 @@ export default function Footer() {
                     </div>
                     <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
                         <div className="space-y-3">
-                            <h3 className="tracking-wide uppercase text-gray-900">Product</h3>
-                            <ul className="space-y-1">
+                            <h3 className="tracking-wide uppercase text-gray-900">Website</h3>
+                            <ul className="space-y-1 ">
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Features</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="/">Home</a>
                                 </li>
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Integrations</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="/blog">About</a>
                                 </li>
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Pricing</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="/location">Property for rent</a>
                                 </li>
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">FAQ</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="/Contact">Contact</a>
                                 </li>
                             </ul>
                         </div>
@@ -36,13 +36,10 @@ export default function Footer() {
                             <h3 className="tracking-wide uppercase text-gray-900">Company</h3>
                             <ul className="space-y-1">
                                 <li>
-                                    <a rel="noopener noreferrer" href="/cgv">cgv</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="/cgv">cgv</a>
                                 </li>
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Privacy</a>
-                                </li>
-                                <li>
-                                    <a rel="noopener noreferrer" href="#">Terms of Service</a>
+                                    <a rel="noopener hover:text-teal-500 noreferrer" href="#">Terms of Service</a>
                                 </li>
                             </ul>
                         </div>
@@ -51,10 +48,10 @@ export default function Footer() {
                             <div className="flex justify-start space-x-3">
                                 {/* Instagram */}
                                 <a
-                                rel="noopener noreferrer"
+                                rel="noopener  noreferrer"
                                 href="https://www.instagram.com/expatlife.com_/"
                                 title="Instagram"
-                                className="flex items-center p-1"
+                                className="flex items-center hover:text-teal-500 p-1"
                                 >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -71,7 +68,7 @@ export default function Footer() {
                                 rel="noopener noreferrer"
                                 href="https://wa.me/971568127898?text=prout"
                                 title="WhatsApp"
-                                className="flex items-center p-1"
+                                className="flex items-center hover:text-teal-500 p-1"
                                 >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -85,17 +82,15 @@ export default function Footer() {
                             </div>
                         </div>
                         <div className="space-y-3">
-                            <h3 className="uppercase text-gray-900">Developers</h3>
+                            <h3 className="uppercase text-gray-900">ExpatLife</h3>
                             <ul className="space-y-1">
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Public API</a>
+                                    <a rel="noopener noreferrer" href="#">meydan, Dubai</a>
                                 </li>
                                 <li>
-                                    <a rel="noopener noreferrer" href="#">Documentation</a>
+                                    <a rel="noopener noreferrer" className=" hover:text-teal-500" href="mailto:contact@expatlife-uae.com">Contact@expatlife-uae.com</a>
                                 </li>
-                                <li>
-                                    <a rel="noopener noreferrer" href="#">Guides</a>
-                                </li>
+                                
                             </ul>
                         </div>
                     </div>
