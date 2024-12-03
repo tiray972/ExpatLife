4adfad633132a178e170a18fd2f09df106063bd6
tiray972 <tiray972@gmail.com>
2024-12-03 11:37:34 +0100

    update: notif test page

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
 self.addEventListener('push', (event) => {
-    const data = event.data.json();
+    let data;
+    try {
+      data = event.data ? event.data.json() : {};
+    } catch (e) {
+      console.error('Error parsing push event data:', e);
+      return; // Stop processing this event if data is invalid
+    }
+
     const options = {
       body: data.body,
-      icon: data.icon || '/icon.png',
-      badge: '/badge.png',
+      icon: data.icon || '/icons/icon-512x512.png',
+      badge: '/icons/icon-512x512.png',
       vibrate: [100, 50, 100],
       data: { primaryKey: '2' },
     };
